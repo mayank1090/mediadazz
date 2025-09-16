413ddbcf9cdf5641f101a6479c06ce3e7b5d2722
Mayank Paliwal <pali28913@gmail.com>
2025-09-16 22:32:01 +0530

    final

--- src/components/ProductDetail/productmap.tsx
+++ src/components/ProductDetail/productmap.tsx
@@ -7,7 +7,7 @@ import L from 'leaflet';
 import { RiExternalLinkLine } from "react-icons/ri";
 
 // Fix for default markers in react-leaflet
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+delete (L.Icon.Default.prototype as L.Icon.Default & { _getIconUrl?: () => string })._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
