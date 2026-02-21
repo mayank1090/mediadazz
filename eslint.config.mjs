912604cde96e48763bd8f72318732668d0d5faa6
mayank10 <pali28913@gmail.com>
2026-02-21 19:04:28 +0530

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,15 +26,7 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
     },
-  },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  }
 ];
  
 export default eslintConfig
